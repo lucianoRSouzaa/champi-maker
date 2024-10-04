452547e3f35a8ab790c3dc95223f171ed0f7c5a6
Luciano <lucianorodriguess101@gmail.com>
2024-10-04 19:25:06 -0300

    feat(repository): add UserRepository interface

---
+++ internal/domain/repository/user_repository.go
@@ -0,0 +1,16 @@
+package repository
+
+import (
+	"champi-maker/internal/domain/entity"
+	"context"
+
+	"github.com/google/uuid"
+)
+
+type UserRepository interface {
+	Create(ctx context.Context, user *entity.User) error
+	GetByID(ctx context.Context, id uuid.UUID) (*entity.User, error)
+	GetByEmail(ctx context.Context, email string) (*entity.User, error)
+	Update(ctx context.Context, user *entity.User) error
+	Delete(ctx context.Context, id uuid.UUID) error
+}
