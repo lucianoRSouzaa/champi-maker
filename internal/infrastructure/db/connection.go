4275e624b63446b16cea7c763de9f2edbf773126
Luciano <lucianorodriguess101@gmail.com>
2024-10-04 19:23:20 -0300

    feat(db): add pgx/v5 package for PostgreSQL database connection

---
+++ internal/infrastructure/db/connection.go
@@ -0,0 +1,26 @@
+package database
+
+import (
+	"context"
+	"time"
+
+	_ "github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func NewPgxPool(connectionString string) (*pgxpool.Pool, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	config, err := pgxpool.ParseConfig(connectionString)
+	if err != nil {
+		return nil, err
+	}
+
+	pool, err := pgxpool.NewWithConfig(ctx, config)
+	if err != nil {
+		return nil, err
+	}
+
+	return pool, nil
+}
