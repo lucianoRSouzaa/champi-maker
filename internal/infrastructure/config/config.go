72af49d7c8de575ec04ff1e62f6dbc6668b2dd0f
Luciano <lucianorodriguess101@gmail.com>
2024-10-08 21:38:08 -0300

    feat(config): add config package with environment variable loading functions

---
+++ internal/infrastructure/config/config.go
@@ -0,0 +1,30 @@
+package config
+
+import (
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/joho/godotenv"
+)
+
+func LoadEnv() error {
+	envPath := filepath.Join("..", "..", "..", ".env")
+	err := godotenv.Load(envPath)
+	if err != nil {
+		log.Println("Erro ao carregar o arquivo .env, usando variáveis de ambiente do sistema")
+	}
+	return err
+}
+
+func GetEnv(key string) string {
+	return os.Getenv(key)
+}
+
+func GetRequiredEnv(key string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		log.Fatalf("A variável de ambiente %s não está definida", key)
+	}
+	return value
+}
